82840a080a29630e5e62013b9d10c144e4390ab4
ariful4082 <ariful4082@gmail.com>
2023-01-19 00:44:49 +0600

    recurtion time and space complexity

--- recursion.js
+++ recursion.js
@@ -30,4 +30,27 @@ function sumOfNumber(n){
     return n + sumOfNumber(n-1)
 }
 
-console.log(sumOfNumber(10))
+// console.log(sumOfNumber(10))
+
+
+// function sumOfArray(arr){
+//     if(arr.length === 0) return 0
+
+//     let rest = arr.slice(1);
+    
+//     return arr[0]+ sumOfArray(rest)
+// }
+
+// console.log(sumOfArray([2,3,5]))
+
+function sumOfArray(arr){
+    return helperSum(arr, 0)
+}
+
+function helperSum(arr, index){
+    if(arr.length === index) return 0;
+
+     return arr[index]+ helperSum(arr, index+1)
+}
+
+console.log(sumOfArray([2,3,5, 10, 10]))
