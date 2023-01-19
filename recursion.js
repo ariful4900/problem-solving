4cc34bf13af1b366f1f330938a3e3a9f558646e3
ariful4082 <ariful4082@gmail.com>
2023-01-19 08:41:58 +0600

    recurtion bigO notation

--- recursion.js
+++ recursion.js
@@ -12,7 +12,6 @@
 
 // countDown(10);
 
-
 // function sumOfNumber(n){
 //     let sum = 0;
 //     for(let i = n; i>=0; i--){
@@ -22,35 +21,46 @@
 // }
 // console.log(sumOfNumber(10));
 
-function sumOfNumber(n){
-
-    if(n<0){
-        return 0;
-    }
-    return n + sumOfNumber(n-1)
-}
+// function sumOfNumber(n) {
+//   if (n < 0) {
+//     return 0;
+//   }
+//   return n + sumOfNumber(n - 1);
+// }
 
 // console.log(sumOfNumber(10))
 
-
-// function sumOfArray(arr){
-//     if(arr.length === 0) return 0
-
-//     let rest = arr.slice(1);
-    
-//     return arr[0]+ sumOfArray(rest)
-// }
+function sumOfArraySloe(arr) {
+  if (arr.length === 0) return 0;
+  
+  let rest = arr.slice(1);
+  
+  return arr[0] + sumOfArraySloe(rest);
+}
 
 // console.log(sumOfArray([2,3,5]))
 
-function sumOfArray(arr){
-    return helperSum(arr, 0)
+function sumOfArrayFast(arr) {
+    return helperSum(arr, 0);
 }
 
-function helperSum(arr, index){
-    if(arr.length === index) return 0;
-
-     return arr[index]+ helperSum(arr, index+1)
+function helperSum(arr, index) {
+    if (arr.length === index) return 0;
+    
+    return arr[index] + helperSum(arr, index + 1);
 }
 
-console.log(sumOfArray([2,3,5, 10, 10]))
+// console.log(input)
+let input = new Array(900).fill(1);
+
+let now = Date.now();
+console.log(sumOfArraySloe(input));
+let finsh = Date.now();
+
+console.log(`time elapsed for slow function:${ finsh - now}`);
+
+now = Date.now();
+console.log(sumOfArrayFast(input));
+finsh = Date.now();
+
+console.log(`time elapsed for fast function:${finsh - now}`);
